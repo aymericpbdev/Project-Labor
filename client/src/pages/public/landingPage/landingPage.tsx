eba06337f3e99aed5b7355da43e6592c3c4f1c56
morgandevmh <morgandevmh@users.noreply.github.com>
2026-05-04 11:04:30 +0200

    feat: ajout section story imgs +css

--- client/src/pages/public/landingPage/landingPage.tsx
+++ client/src/pages/public/landingPage/landingPage.tsx
@@ -3,6 +3,9 @@ import HowItWorks from './HowItWorks/HowItWorks'
 import Story from './Story/Story'
 import LatestJobs from './LatestJobs/LatestJobs'
 import FinalCTA from './FinalCTA/FinalCTA'
+import StoryIMG1 from '../../../assets/img-landingpage/storyIMG1.jpg'
+import StoryIMG2 from '../../../assets/img-landingpage/storyIMG2.jpg'
+
 
 import './landingPage.css'
 
@@ -40,7 +43,7 @@ function LandingPage() {
       variant='left' 
       title='lorem ipsum' 
       text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis tortor orci, vel luctus eros pharetra a. Mauris non ultrices nulla. Quisque quis rhoncus nunc. Donec malesuada facilisis lacus, nec viverra justo. Ut quis lacus non sem rutrum hendrerit. Pellentesque consectetur ornare dolor euismod eleifend. Integer suscipit vel metus vitae sollicitudin. Vivamus faucibus, ex sed vehicula fringilla, est lorem luctus nisi, at volutpat libero purus vel ex.'
-      image=''
+      image={StoryIMG1}
       imageAlt='belle image de vigne'
       subtitle='lorem ipsum'
       subtext='lorem ipsum'
@@ -49,7 +52,7 @@ function LandingPage() {
       variant='right' 
       title='lorem ipsum' 
       text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis tortor orci, vel luctus eros pharetra a. Mauris non ultrices nulla. Quisque quis rhoncus nunc. Donec malesuada facilisis lacus, nec viverra justo. Ut quis lacus non sem rutrum hendrerit. Pellentesque consectetur ornare dolor euismod eleifend. Integer suscipit vel metus vitae sollicitudin. Vivamus faucibus, ex sed vehicula fringilla, est lorem luctus nisi, at volutpat libero purus vel ex.'
-      image=''
+      image={StoryIMG2}
       imageAlt='belle image de champ'
       subtitle='lorem ipsum'
       subtext='lorem ipsum'
