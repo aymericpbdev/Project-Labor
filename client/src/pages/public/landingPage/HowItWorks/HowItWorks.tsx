0c677e19e620be6e2fff7a86123cb8cfc35354e8
morgandevmh <morgandevmh@users.noreply.github.com>
2026-05-05 11:00:00 +0200

    feat: ajout section howitworks css

--- client/src/pages/public/landingPage/HowItWorks/HowItWorks.tsx
+++ client/src/pages/public/landingPage/HowItWorks/HowItWorks.tsx
@@ -6,21 +6,18 @@ function HowItWorks() {
       <h2 className="how-it-works__title">Comment ça marche</h2>
 
       <div className="how-it-works__steps">
-        <div className="how-it-works__step">
+        <div className="how-it-works__step step-1">
           {/* TODO : icône épis */}
-          <svg className="how-it-works__icon" />
           <h3 className="how-it-works__step-title">Créez votre profil</h3>
         </div>
 
-        <div className="how-it-works__step">
+        <div className="how-it-works__step step-2">
           {/* TODO : icône épis */}
-          <svg className="how-it-works__icon" />
           <h3 className="how-it-works__step-title">Connectez-vous</h3>
         </div>
 
-        <div className="how-it-works__step">
+        <div className="how-it-works__step step-3">
           {/* TODO : icône épis */}
-          <svg className="how-it-works__icon" />
           <h3 className="how-it-works__step-title">Publiez ou cherchez</h3>
         </div>
       </div>
